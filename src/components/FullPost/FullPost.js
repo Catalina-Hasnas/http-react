6019b926ead3538edc8dde191068f2ed34ad62ed
Catalina Hasnas <catalinahasnas1996@gmail.com>
2020-11-11 12:53:41 +0200

    module finished

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -13,7 +13,7 @@ class FullPost extends Component {
     componentDidUpdate() {
         if (this.props.id) {
             if ( !this.state.loadedPost || (this.state.loadedPost.id !== this.props.id)) {
-                axios.get("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
+                axios.get("/posts/" + this.props.id)
             .then(response => {
                 this.setState({
                     loadedPost: response.data
@@ -22,6 +22,13 @@ class FullPost extends Component {
             }
             
         }
+    }
+
+    deletePostHandler = () => {
+        axios.delete("/posts/" + this.props.id)
+            .then(response => {
+                console.log(response)
+            })
     }
 
     render () {
@@ -37,7 +44,7 @@ class FullPost extends Component {
                     <h1>{this.state.loadedPost.title}</h1>
                     <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button onClick={this.deletePostHandler} className="Delete">Delete</button>
                     </div>
                 </div>
     
